6204932e4c6efb5fe67cc65eb4f48406746fb0fb
alizy <alizy@SAKSOOOK>
2024-03-21 20:11:52 +0200

    work on the view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using CarBill.bussinesData;
 using CarBill.Models;
+using CarBill.vm;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CarBill.Controllers
@@ -25,10 +27,28 @@ namespace CarBill.Controllers
         // get end points format => APIName/id
         public ActionResult ShowCarData(int id)
         {
-            // get the carData
-            var carData = _db.Cars.FirstOrDefault(i => i.Id == id);
+            BillFormData billFormData = new BillFormData();
 
-            return View(carData);
+            // get the carData in BillFormData
+            var cardata = _db.Cars.FirstOrDefault(i => i.Id == id);
+            billFormData.carData = cardata;
+
+            // get the categories
+            var Cats = _db.MaintananceTypes.ToList();
+            billFormData.cats = Cats;
+
+            // get the spare Parts
+            var spareParts = _db.SpareParts.Include(s => s.Type).ToList();
+            billFormData.sparePartsWithCat = spareParts;
+
+            return View(billFormData);
+        }
+
+        // API endpoint
+        public ActionResult GetSparePartsByCategory(int categoryId)
+        {
+            var spareParts = _db.SpareParts.Where(sp => sp.TypeId == categoryId).ToList();
+            return Ok(spareParts);
         }
 
         public IActionResult yourBills()
---
+++ vm/BillFormData.cs
@@ -0,0 +1,35 @@
+﻿using CarBill.bussinesData.Models;
+
+namespace CarBill.vm
+{
+    public class BillRowWithImagesForm
+    {
+        public int billRowId { get; set; }
+        public int amount { get; set; }
+        public decimal total_priceForBillRow { get; set; }
+        public int billId { get; set; }
+        public int sparePartId { get; set; }
+        public List<FormFile> PhotosInFormFiles { get; set; }
+
+
+
+    }
+    public class AdditionBillData
+    {
+        public List<BillRowWithImagesForm> billRows { get; set; }
+        public string comment { get; set; }
+        public string currentReading { get; set; }
+        public string maintancePeriod { get; set; }
+        public decimal totalPrice { get; set; }
+        public int discountPrecentage { get; set; }
+        public FormFile videoInFormFile { get; set; }
+        
+    }
+    public class BillFormData
+    {
+        public Car carData { get; set; }
+        public List<SparePart> sparePartsWithCat { get; set; }
+        public AdditionBillData additionBillData { get; set; }
+        public List<MaintananceType> cats { get; set; }
+    }
+}
